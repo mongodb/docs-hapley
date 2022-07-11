73308ce56904422eb94b8be9b4495a8e3353d208
Drew Beckmen <drew.beckmen@mongodb.com>
2022-07-11 15:34:51 -0400

    deploy frontend and backend on same domain to solve cors issues

--- frontend/src/pages/request-poc.tsx
+++ frontend/src/pages/request-poc.tsx
@@ -1,11 +1,5 @@
 import React from "react";
 
-const unauthenticatedRequest = (url: string) => {
-    fetch(url)
-        .then(res => res.text())
-        .then(text => console.log(text))
-}
-
 const authenticatedRequest = (url: string) => {
     fetch(url)
     .then(res => res.json())
@@ -15,22 +9,16 @@ const authenticatedRequest = (url: string) => {
 const RequestPOC = () => {
     return (
         <div>
-            <h1>Request POC</h1>
+            <h1>Authenticated Request POC</h1>
             <p>
-                This is a POC for sending authenticated requests to the API.
+                This is a POC for sending authenticated requests to the API. We consolidated the frontend and backend
+                on a single ingress domain to get around CORS limitations that come with CorpSecure authentication in Kanopy.
             </p>
-            
-            <h2>Without including cookies in request:</h2>
-            <p>
-                Click the button to send a request to the API without cookies. Check the console for the response. It should be raw HTML for the MongoDB corp sign in page.
-            </p>
-            <button onClick={() => unauthenticatedRequest("https://hapley-api.docs.staging.corp.mongodb.com/hello")}>Send unauthenticated request</button>
 
-            <h2>With including cookies in request:</h2>
             <p>
-                Click the button to send a request to the API with cookies. Check the console for the response. It should be a JSON object with the message "Hello World".
+                Click the button to send a request to the API. Check the console for the response. It should be a JSON object with the message "Hello World".
             </p>
-            <button onClick={() => authenticatedRequest("http://localhost:8000/hello")}>Send authenticated request</button>  
+            <button onClick={() => authenticatedRequest(`${process.env.GATSBY_API_URL}/hello`)}>Send authenticated request</button>
         </div>
     )
 };
