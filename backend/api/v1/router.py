c899a317f91a68fec1d2fca8a7d6805658254891
Drew Beckmen <drew.beckmen@mongodb.com>
2022-07-12 17:09:28 -0400

    remove unnecessary files

--- backend/api/v1/router.py
+++ backend/api/v1/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
-from .routes import repos, hello_world
+from .routes import hello_world
 
 router = APIRouter()
-router.include_router(repos.router, prefix='/repos', tags=['repos'])
 router.include_router(hello_world.router)
