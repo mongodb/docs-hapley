cfb27d92d127268f6c0b5e6ce877a52f967c8000
rayangler <27821750+rayangler@users.noreply.github.com>
2022-07-21 18:56:01 -0400

    Create route for put

--- backend/api/model/repo.py
+++ backend/api/model/repo.py
@@ -1,4 +1,5 @@
 from beanie import Document
+from fastapi import HTTPException, status
 from pydantic import BaseModel, Field
 
 
@@ -36,13 +37,38 @@ class Repo(Document):
     groups: list[Group] | None
 
 
-class RepoGroupView(BaseModel):
+class RepoGroupsView(BaseModel):
     """Projection focused on groups for a specific repo."""
 
-    name: str = DefaultRepoFields.name
     groups: list[Group] | None
 
 
-async def insert_new_group(repo_name: str, group: Group):
+class RepoNotFound(HTTPException):
+    def __init__(self, repo_name: str) -> None:
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"The repo {repo_name} does not exist.",
+        )
+
+
+async def find_one_repo(repo_name: str) -> Repo | None:
     repo = await Repo.find_one(Repo.name == repo_name)
+    return repo
+
+
+async def insert_new_group(repo_name: str, group: Group) -> None:
+    repo = await find_one_repo(repo_name)
+
+    if not repo:
+        raise RepoNotFound(repo_name)
+
     await repo.update({"$push": {"groups": group}})
+
+
+async def set_groups(repo_name: str, groups: list[Group]) -> None:
+    repo = await find_one_repo(repo_name)
+
+    if not repo:
+        raise RepoNotFound(repo_name)
+
+    await repo.update({"$set": {"groups": groups}})
--- backend/api/v1/routes/groups.py
+++ backend/api/v1/routes/groups.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from api.model.repo import Repo, RepoGroupView, Group, insert_new_group
+from api.model.repo import Repo, RepoGroupsView, Group, insert_new_group, set_groups
 from api.dependencies import check_if_user_entitled_to_repo
 
 router = APIRouter()
@@ -8,17 +8,23 @@ router = APIRouter()
 
 @router.get(
     "/",
-    response_model=RepoGroupView,
+    response_model=RepoGroupsView,
     dependencies=[Depends(check_if_user_entitled_to_repo)],
 )
 async def read_groups(repo_name: str):
-    return await Repo.find_one(Repo.name == repo_name).project(RepoGroupView)
+    return await Repo.find_one(Repo.name == repo_name).project(RepoGroupsView)
 
 
 @router.post(
     "/",
+    response_model=Group,
     dependencies=[Depends(check_if_user_entitled_to_repo)],
 )
 async def create_group(repo_name: str, group: Group):
     await insert_new_group(repo_name, group)
     return group
+
+
+@router.put("/", dependencies=[Depends(check_if_user_entitled_to_repo)])
+async def update_groups(repo_name: str, body: RepoGroupsView):
+    await set_groups(repo_name, body.groups)
