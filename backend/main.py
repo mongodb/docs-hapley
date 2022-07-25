0a8df49e5ec8c4eec371128d384ffaef1edc467a
rayangler <27821750+rayangler@users.noreply.github.com>
2022-07-25 16:07:53 -0400

    Use get_settings()

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
-from api.core.config import Settings
+from api.core.config import get_settings
 from api.core.factory import create_app
 from api.database import start_db_client
 
-settings = Settings()
+settings = get_settings()
 app = create_app(settings)
 
 
