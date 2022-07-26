24759408418740e440e3ff7431caff4da0b536f9
rayangler <27821750+rayangler@users.noreply.github.com>
2022-07-26 10:21:51 -0400

    Fix merge conflicts - extra files

--- backend/api/tests/unit/routes/test_repos.py
+++
@@ -1,8 +0,0 @@
-from api.tests.base import FastApiTest
-
-
-def test_repos_get():
-    with FastApiTest() as client:
-        response = client.get("/repos")
-        assert response.status_code == 200
-        assert response.json()["repos"] == ["docs-landing"]
